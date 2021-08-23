8a40088bc796ed37c42c14bc0e903ea3f8a7e00e
Paul Frazee <pfrazee@gmail.com>
2021-08-23 11:27:41 -0500

    Remove old json-rpc hyper apis

--- src/hyper/service.ts
+++ src/hyper/service.ts
@@ -13,23 +13,7 @@ import ram from 'random-access-memory'
 import WebSocket, { createWebSocketStream } from 'ws'
 import { ServiceInstance, ServiceConfig } from '../services/instance.js'
 import AtekService, { ServiceManifest } from '../gen/atek.cloud/service.js'
-import HypercoreApiServer from '../gen/atek.cloud/hypercore-api.server.js'
-import PingApiServer from '../gen/atek.cloud/ping-api.server.js'
-import {
-  CreateResponse,
-  DescribeResponse,
-  GetOptions,
-  DownloadOptions,
-  UpdateOptions,
-  SeekResponse,
-  ConfigureNetworkOptions
-} from '../gen/atek.cloud/hypercore-api.js'
 import * as apiBroker from '@atek-cloud/api-broker'
-
-interface APIs {
-  hyper: HypercoreApiServer
-  ping: PingApiServer
-}
 
 interface HyperDHT extends EventEmitter {
   listen: () => void
@@ -41,21 +25,11 @@ export class HyperServiceInstance extends ServiceInstance {
   server?: HyperspaceServer
   client?: HyperspaceClient
   dht?: HyperDHT
-  apis: APIs
   hypers: QuickLRU<string, RemoteHypercore>
 
   constructor (settings: AtekService, config: ServiceConfig) {
     super(settings)
     this.setConfig(config)
-    this.apis = {
-      hyper: this.createHyperApi(),
-      ping: new PingApiServer({
-        ping (num: number): Promise<number> {
-          console.log('Hyper got ping', num)
-          return Promise.resolve(num)
-        }
-      })
-    }
     this.hypers = new QuickLRU({maxSize: 1000})
   }
 
@@ -66,7 +40,6 @@ export class HyperServiceInstance extends ServiceInstance {
       license: 'MIT',
       exports: [
         {api: 'atek.cloud/hypercore-api'},
-        {api: 'atek.cloud/ping-api'}
       ]
     }
   }
@@ -160,12 +133,6 @@ export class HyperServiceInstance extends ServiceInstance {
   }
 
   handleRpc (callDesc: apiBroker.CallDescription, methodName: string, params: unknown[]): Promise<unknown> {
-    if (callDesc.api === 'atek.cloud/hypercore-api') {
-      return this.apis.hyper.handle(callDesc, methodName, params)
-    }
-    if (callDesc.api === 'atek.cloud/ping-api') {
-      return this.apis.ping.handle(callDesc, methodName, params)
-    }
     throw new apiBroker.ServiceNotFound('API not found')
   }
 
@@ -177,103 +144,5 @@ export class HyperServiceInstance extends ServiceInstance {
     } else {
       throw new apiBroker.ServiceNotFound('API not found')
     }
-  }
-
-  createHyperApi () {
-    const getCore = (key: Uint8Array): RemoteHypercore => {
-      if (!key) throw new Error('Hypercore key is required')
-      const keyBuf = Buffer.from(key)
-      if (keyBuf.byteLength !== 32) throw new Error('Hypercore key must be 32-bytes')
-
-      if (!this.client) throw new Error('Hypercore service not active')
-
-      const keyStr = keyBuf.toString('hex')
-      if (!this.hypers.has(keyStr)) {
-        this.hypers.set(keyStr, this.client.corestore().get(keyBuf))
-      }
-      const core = this.hypers.get(keyStr)
-      if (!core) throw new Error('Failed to get core')
-      return core
-    }
-
-    const self = this
-    return new HypercoreApiServer({
-      async create (): Promise<CreateResponse> {
-        if (!self.client) throw new Error('Hypercore service not active')
-        // @ts-ignore For some reason, my TS declarations don't like this line
-        const core = self.client.corestore().get(undefined)
-        await core.ready()
-        return {
-          key: core.key,
-          discoveryKey: core.discoveryKey,
-          writable: core.writable,
-          length: core.length,
-          byteLength: core.byteLength
-        }
-      },
-  
-      async describe (key: Uint8Array): Promise<DescribeResponse> {
-        const core = getCore(key)
-        await core.ready()
-        return {
-          key: core.key,
-          discoveryKey: core.discoveryKey,
-          writable: core.writable,
-          length: core.length,
-          byteLength: core.byteLength
-        }
-      },
-      
-      append (key: Uint8Array, data: Uint8Array | Uint8Array[]): Promise<number> {
-        const core = getCore(key)
-        return core.append(Array.isArray(data) ? data.map(d => Buffer.from(d)) : Buffer.from(data))
-      },
-      
-      get (key: Uint8Array, index: number, options: GetOptions): Promise<Uint8Array> {
-        const core = getCore(key)
-        return core.get(index, options)
-      },
-      
-      cancel (key: Uint8Array, getCallId: number): Promise<void> {
-        // TODO
-        return Promise.resolve(undefined)
-      },
-      
-      has (key: Uint8Array, index: number): Promise<boolean> {
-        const core = getCore(key)
-        return core.has(index)
-      },
-      
-      download (key: Uint8Array, start: number, end: number, options: DownloadOptions): Promise<void> {
-        const core = getCore(key)
-        return core.download(start, end)
-      },
-      
-      undownload (key: Uint8Array, downloadCallId: number): Promise<void> {
-        // TODO
-        return Promise.resolve(undefined)
-      },
-      
-      downloaded (key: Uint8Array, start: number, end: number): Promise<number> {
-        const core = getCore(key)
-        return core.downloaded(start, end)
-      },
-      
-      update (key: Uint8Array, opts: UpdateOptions): Promise<void> {
-        const core = getCore(key)
-        return core.update(opts)
-      },
-      
-      seek (key: Uint8Array, byteOffset: number): Promise<SeekResponse> {
-        const core = getCore(key)
-        return core.seek(byteOffset)
-      },
-      
-      configureNetwork (key: Uint8Array, opts: ConfigureNetworkOptions): Promise<void> {
-        const core = getCore(key)
-        // @ts-ignore For some reason, my TS declarations don't like this line
-        return this.client.network.configure(core, opts)
-      }
-    })
   }
 }
