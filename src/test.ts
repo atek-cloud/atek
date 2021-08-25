5bf9f70d6e28bf05db02a1da136946b48ca15825
Paul Frazee <pfrazee@gmail.com>
2021-08-25 11:36:48 -0500

    Fix: use the correct command to start the test server

--- src/test.ts
+++ src/test.ts
@@ -67,13 +67,14 @@ export async function startAtek (config: Config = new Config()) {
   const cfgDir = tmp.dirSync({unsafeCleanup: true})
   const cfgPath = path.join(cfgDir.name, 'config.json')
   const authToken = generateBearerToken()
+  config.port = PORT
   config.systemAuthTokens = [authToken]
   fs.writeFileSync(cfgPath, JSON.stringify(config))
 
   const binPath = path.join(path.dirname(fileURLToPath(import.meta.url)), 'bin.js')
   const serverProcess = spawn(
     'node',
-    [binPath, 'start', '--configDir', cfgDir.name, '--port', String(PORT)],
+    [binPath, 'run', '--configDir', cfgDir.name, '--port', String(PORT)],
     {
       stdio: [process.stdin, process.stdout, process.stderr],
       env: INSPECTOR_ENABLED ? Object.assign({}, process.env, {NODE_OPTIONS: `--inspect=localhost:${PORT-1}`}) : undefined
