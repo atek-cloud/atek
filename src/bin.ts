b3cbd4fd83ac04e2bd9161cccb7d7b5a5da4370a
Paul Frazee <pfrazee@gmail.com>
2021-08-30 11:33:29 -0500

    Add help/usage to the CLI

--- src/bin.ts
+++ src/bin.ts
@@ -13,6 +13,12 @@ import { createApi } from './lib/rpc.js'
 
 const PACKAGE_JSON_PATH = path.join(path.dirname(fileURLToPath(import.meta.url)), '..', 'package.json')
 
+const RUN_OPTS = [
+  {name: 'port', help: 'Set the port to run the server on (defaults to 80)'},
+  {name: 'domain', help: 'Set the domain the server will be accessed by (defaults to "localhost")'},
+  {name: 'configDir', help: 'Set the directory to read configuration from (defaults to "~/.atek")'}
+]
+
 function runCommand (args: any): void {
   server.start({
     port: args.port,
@@ -27,21 +33,64 @@ async function apiCall (args: any, apiId: string, method: string, params: any[])
   try {
     return await api(method, params)
   } catch (e) {
-    console.error(e)
+    if (e.code === 'ECONNREFUSED') {
+      console.error('Failed to connect to Atek.')
+      console.error('Make sure Atek is running before calling this command:')
+      console.error('')
+      console.error('  atek run')
+      console.error('')
+    } else {
+      console.error(e)
+    }
     process.exit(1)
   }
 }
 
-const match = subcommand({
+function usage (args: any, help: string, usage: string) {
+  console.log(help)
+  if (usage) {
+    console.log('')
+    console.log(usage)
+  }
+}
+
+const cmdOpts = {
+  usage: {
+    help: 'atek - Run the atek server',
+    option: {
+      name: 'help',
+      abbr: 'h'
+    },
+    command: function (args: any, help: string, usage: string) {
+      console.log(help)
+      console.log('')
+      console.log(usage)
+      console.log('')
+      console.log('Commands:')
+      for (const cmd of cmdOpts.commands) {
+        console.log('  ', cmd.help || `atek ${cmd.name}`)
+      }
+    }
+  },
   commands: [
     {
       name: 'run',
+      help: 'atek run - Run the atek server',
+      options: RUN_OPTS,
+      usage,
       command: runCommand
     },
     {
       name: 'install',
+      help: 'atek install {url_or_path} - Install a new service',
+      usage,
       command: async (args: any) => {
         if (args._[0]) args.sourceUrl = args._[0]
+        if (!args.sourceUrl) {
+          console.error('URL or path is required:')
+          console.error('atek install {url_or_path}')
+          process.exit(1)
+        }
         if (args.sourceUrl.startsWith('/')) {
           args.sourceUrl = `file://${args.sourceUrl}`
         }
@@ -50,18 +99,24 @@ const match = subcommand({
     },
     {
       name: 'uninstall',
+      help: 'atek uninstall {id} - Uninstall a service',
+      usage,
       command: async (args: any) => {
         await apiCall(args, 'atek.cloud/services-api', 'uninstall', [args.id || args._[0]])
       }
     },
     {
       name: 'update',
+      help: 'atek update {id} - Update a service installation from its source',
+      usage,
       command: async (args: any) => {
         console.log(await apiCall(args, 'atek.cloud/services-api', 'updatePackage', [args.id || args._[0]]))
       }
     },
     {
       name: 'ls',
+      help: 'atek ls - List active services',
+      usage,
       command: async (args: any) => {
         const {services} = await apiCall(args, 'atek.cloud/services-api', 'list', [])
         const out: NodeJS.Dict<object> = {}
@@ -78,6 +133,8 @@ const match = subcommand({
     },
     {
       name: 'get',
+      help: 'atek get {id} - Get info about a service',
+      usage,
       command: async (args: any) => {
         const srv = await apiCall(args, 'atek.cloud/services-api', 'get', [args.id || args._[0]])
         console.log(srv.settings.id, `(${srv.status})`)
@@ -86,42 +143,66 @@ const match = subcommand({
     },
     {
       name: 'cfg',
+      help: 'atek cfg {id} ... - Configure a service or get its current configuration',
+      usage,
       command: async (args: any) => {
         await apiCall(args, 'atek.cloud/services-api', 'configure', [args.id || args._[0], args])
       }
     },
     {
       name: 'start',
+      help: 'atek start {id} - Start a service',
+      usage,
       command: async (args: any) => {
         await apiCall(args, 'atek.cloud/services-api', 'start', [args.id || args._[0]])
       }
     },
     {
       name: 'stop',
+      help: 'atek stop {id} - Stop a service',
+      usage,
       command: async (args: any) => {
         await apiCall(args, 'atek.cloud/services-api', 'stop', [args.id || args._[0]])
       }
     },
     {
       name: 'restart',
+      help: 'atek restart {id} - Restart a service',
+      usage,
       command: async (args: any) => {
         await apiCall(args, 'atek.cloud/services-api', 'restart', [args.id || args._[0]])
       }
     },
     {
       name: 'tsgen gen-file',
+      help: 'atek tsgen gen-file - Generate typescript from a d.ts file',
+      options: [
+        {name: 'in', help: 'The .d.ts input file path'},
+        {name: 'out', help: 'The path of the folder receiving the output'},
+        {name: 'env', help: 'The target environment. Must be host, node-userland, or deno-userland'}
+      ],
+      usage,
       command: (args: any) => {
         tsgen.doGenerateFile(args)
       }
     },
     {
       name: 'tsgen gen-folder',
+      help: 'atek tsgen gen-folder - Generate typescript from a folder of d.ts files',
+      options: [
+        {name: 'in', help: 'The path of the folder containing the input d.ts files'},
+        {name: 'out', help: 'The path of the folder receiving the output'},
+        {name: 'env', help: 'The target environment. Must be host, node-userland, or deno-userland'}
+      ],
+      usage,
       command: (args: any) => {
         tsgen.doGenerateFolder(args)
       }
     },
     {
       name: 'repl',
+      help: 'atek repl - Connect a repl to the atek instance',
+      usage,
       command: (args: any) => {
         const port = Number(args.port || DEFAULT_REPL_PORT)
         const host = args.host || 'localhost'
@@ -137,6 +218,12 @@ const match = subcommand({
     }
   ],
   root: {
+    name: 'run',
+    help: 'Run the atek server',
+    options: [
+      {name: 'version', abbr: 'v', help: 'Print the current version'},
+      ...RUN_OPTS
+    ],
     command: (args: any) => {
       if (args.v || args.version) {
         const packageJson = fs.readFileSync(PACKAGE_JSON_PATH, 'utf8')
@@ -147,5 +234,6 @@ const match = subcommand({
       }
     }
   }
-})
-const cmd = match(process.argv.slice(2))
+}
+const match = subcommand(cmdOpts)
+const matchedCmd = match(process.argv.slice(2))
